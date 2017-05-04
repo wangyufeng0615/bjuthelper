43343153588b957e36289940ca6640b7e65309d0
wangyufeng0615 <wyf0615@emails.bjut.edu.cn>
2017-05-04 09:22:14 +0800

    fix text problem of login page.

--- login_grade.php
+++ login_grade.php
@@ -130,7 +130,7 @@
 
 	<article class="weui_article">
         <a href="http://www.devchen.com/blog/coding/PHP/20170112-bjut-helper/#2017-1-11">关于更新后加权和绩点变化的说明</a><br>
-		&nbsp;<h1><i class="weui_icon_success_circle"></i>&nbsp;账号和密码不会保留，请放心使用。<br/>说明：总加权平均分和总平均GPA的数据只对没报二专业/辅修的同学有效。如果存在分数不足60分的科目，总加权分数可能不准。<br/>注意：数据仅供参考，请以教务系统为准。<br/><br/>
+		&nbsp;<h1><i class="weui_icon_success_circle"></i>&nbsp;账号和密码不会保留，请放心使用。<br/>说明：总加权平均分和总平均GPA的数据只对<b>没报二专业/辅修</b>的同学有效。如果存在分数不足60分的科目，总加权分数可能不准。<br/><b>注意：</b>数据仅供参考，请以教务系统为准。<br/><br/>
 		<section>
 如数据有问题(或者网站打不开了)请联系<a href="http://www.wangyufeng.org">王雨峰</a><br />
 Contact: wyf0615@emails.bjut.edu.cn<br />
@@ -139,7 +139,7 @@ Contact: i@devChen.com<br />
 代码重构 by 马文瑞<br />
 <a href="http://www.devchen.com/blog/coding/PHP/20170112-bjut-helper/#更新日志">更新日志</a><br>
 <br />
-QR Code is designed by the most beautiful girl in the world.<br />
+QR Code is designed by the most beautiful girl in the world.<br /><br />
 <a href="http://www.miit.gov.cn/">京ICP备16062922号-1</a>
         </section>
     </article>
